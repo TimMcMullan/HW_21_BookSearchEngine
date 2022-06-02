09bbb24156805e22981f7540f174f7c48c74bffe
TimMcMullan <mcmullant@gmail.com>
2022-06-02 09:00:02 -0400

    built out schemas.  added some to server and auth

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+}
+
+type Book {
+    bookId: ID
+    authors: String
+    description: String
+    image: String
+    title: String
+    link: String!
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookInfo: String!): User
+    removeBook(bookInfo: String!): User
+  }
+  `;
+
+  module.exports = typeDefs;
