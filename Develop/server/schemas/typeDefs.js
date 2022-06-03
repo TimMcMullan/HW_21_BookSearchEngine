4c9fe414d5cd4b9cb682fb4500f419e62c8cf427
TimMcMullan <mcmullant@gmail.com>
2022-06-02 20:06:57 -0400

    tweaked typeDefs and index

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -3,16 +3,16 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 type User {
     _id: ID
-    username: String
-    email: String
-    password: String
+    username: String!
+    email: String!
+    password: String!
     savedBooks: [Book]!
 }
 
 type Book {
-    bookId: ID
+    bookId: ID!
     authors: String
-    description: String
+    description: String!
     image: String
     title: String
     link: String!
