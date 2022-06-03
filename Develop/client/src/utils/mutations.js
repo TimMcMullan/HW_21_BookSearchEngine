01f2a4750686fab3b8f17b74869aa8e3cf23af6b
TimMcMullan <mcmullant@gmail.com>
2022-06-03 08:40:59 -0400

    stashing build

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookInfo: String!) {
+    saveBook(bookInfo: $bookInfo) {
+         Book {
+            bookId
+            authors
+            description
+            image
+            title
+            link 
+                }
+      }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation deleteBook($bookInfo: String!) {
+    deleteBook(bookInfo: $bookInfo) {
+         Book {
+            bookId
+            authors
+            description
+            image
+            title
+            link 
+                }
+      }
+  }
+  `;
